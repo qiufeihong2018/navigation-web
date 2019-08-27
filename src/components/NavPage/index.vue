3d117c785e6225ae0540f7fb80f7333330461b2d
qiufeihong2018 <15058301288@163.com>
2019-08-27 22:23:54 +0800

    style(change backtotop svg)

--- src/components/NavPage/index.vue
+++ src/components/NavPage/index.vue
@@ -118,13 +118,13 @@ export default {
       categoryOptions: [],
       // customizable button style, show/hide critical point, return position
       myBackToTopStyle: {
-        right: '50px',
-        bottom: '50px',
-        width: '40px',
-        height: '40px',
-        'border-radius': '4px',
-        'line-height': '45px', // 请保持与高度一致以垂直居中 Please keep consistent with height to center vertically
-        background: '#e7eaf1' // 按钮的背景颜色 The background color of the button
+        right: '35px',
+        bottom: '75px',
+        width: '50px',
+        height: '50px',
+        'border-radius': '50%',
+        'line-height': '50px', // 请保持与高度一致以垂直居中 Please keep consistent with height to center vertically
+        // background: '#409EFF' // 按钮的背景颜色 The background color of the button
       },
       currentRoute: this.$router.currentRoute.path.split('/'),
       loading: true,
